f6ddc6123891737f3e710b4c17f749ea531ed13c
Anton Grishin <anton_grishin@epam.com>
2022-02-16 17:44:52 +0300

    Print emoji

--- solution.go
+++ solution.go
@@ -1,5 +1,14 @@
-package solution
+package main
+
+import "fmt"
+import "github.com/kyokomi/emoji"
 
 func GetMessage() string {
+	emoji_msg := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(emoji_msg)
 	return ""
+}
+
+func main() {
+	GetMessage()
 }
