2f3e9c35695ec134308853c9aace05a1de6ae36b
Anton Grishin <anton_grishin@epam.com>
2022-02-16 17:49:17 +0300

    Remove useless func

--- solution.go
+++ solution.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 import "github.com/kyokomi/emoji"
 
-func GetMessage() string {
+func main() {
 	emoji_msg := emoji.Sprint("Hello :world_map:!")
 	fmt.Println(emoji_msg)
-	return ""
-}
-
-func main() {
-	GetMessage()
 }
