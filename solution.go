b579a86373867ed127a7e9f8c02e7c821f4190bb
Anton Grishin <anton_grishin@epam.com>
2022-02-16 18:19:36 +0300

    Rewrite func and test

--- solution.go
+++ solution.go
@@ -4,10 +4,10 @@ import "fmt"
 import "github.com/kyokomi/emoji"
 
 func GetMessage() string {
-	fmt.Println(emoji.Sprint("Hello :world_map:!"))
-	return ""
+	emoji_msg := emoji.Sprint("Hello :world_map:!")
+	return emoji_msg
 }
 
 func main() {
-	GetMessage()
+	fmt.Println(GetMessage())
 }
